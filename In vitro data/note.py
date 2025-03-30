caebc305ea8d41cc91fed0f20fe9b5270b9e7d62
Joshua Pillai <jppillai@ucsd.edu>
2025-03-30 10:19:00 -0700

    Create note.py

---
+++ In vitro data/note.py
@@ -0,0 +1,5 @@
+## The source data from Marsh et al., (2025) for the respective mean Abeta levels is correct in its original format to the plots that are shown in the bar graphs displayed in the publication.
+
+## However, for Hsu et al., (2020), the mean Abeta levels shown in the data file is the transformed version of the data after the difference was taken from the WT values at 1. If the mean MT value is negative, we add 1 to the value, but if the value is negative, we substract by 1.
+
+## In this folder, we have provided the raw data provided from these respective publications, along with their significance.
